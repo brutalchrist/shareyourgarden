79cec6835eebe1287d671fd549abbed9d8e5de62
brutalchrist <brutalchrist@gmail.com>
2018-03-05 18:11:38 -0300

    Persons service

--- frontend/src/client/app/services/services.module.ts
+++ frontend/src/client/app/services/services.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { PersonService } from './person/person.service';
+import { PersonsService } from './persons/persons.service';
 import { RestService } from './rest/rest.service';
 import { MapService } from './map/map.service';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,7 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [CommonModule, RouterModule],
     declarations: [],
-    providers:[MapService, RestService, PersonService],
+    providers:[MapService, RestService, PersonsService],
     exports: [CommonModule, FormsModule, RouterModule]
 })
 export class ServicesModule {
